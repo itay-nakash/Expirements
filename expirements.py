1fef5a4de4b6512aa54b716fa9756c4b31b786d7
Itay Nakash <itay.nakash@nlp16.iem.technion.ac.il>
2022-11-16 17:56:52 +0200

    finished similarity-between-layers exp

--- expirements.py
+++ expirements.py
@@ -1,5 +1,4 @@
 import itertools
-import json
 from calendar import c
 from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
@@ -11,13 +10,12 @@ import bert_score
 from torch import Tensor, logit
 from transformers import AutoModelForMaskedLM, AutoTokenizer, pipeline
 
+import utils
+
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 model = AutoModelForMaskedLM.from_pretrained("roberta-base")
 MIN_NUM_OF_SEN = 10
-
-def read_dict_from_json(fname):
-    with open(fname) as json_file:
-        return json.load(json_file)
+STATES_NUM= 13
 
 # for batch tensors:
 def cosine_similarity(tensor1: Tensor, tensor2: Tensor, eps: float = 1e-8):
@@ -65,7 +63,8 @@ def convert_sentences_list_to_model_input(senteneces: List[List[str]]):
 
 def compare_layers_similarities(senteneces: List[List[str]],mask_loc: int, masked_word: str):
     input_ids_padded = convert_sentences_list_to_model_input(senteneces)
-
+    #layers_sim[i][j] -> the similarity between layers i,j when 
+    layers_sim=np.ones((13,13))
     with torch.no_grad():
         outputs = model(**input_ids_padded, output_hidden_states=True)
 
@@ -78,15 +77,19 @@ def compare_layers_similarities(senteneces: List[List[str]],mask_loc: int, maske
         for j,state2 in enumerate(states):
             # run only on pairs that j<i - so we run once on each:
             if j<i:
+                i_j_means=np.empty(state1.shape[0]) 
                 for k in range(state1.shape[0]):
                     sim = cos_s(state1[k],state2[k])
                     sim = sim.cpu().numpy()
-                    
                     # mean over all the hidden-states dimentions:
                     mean_sim = np.mean(sim)
-                    print(f'the cos_s between layre {i} and layer {j} in the {k}\'s sen is: {mean_sim}')
+                    i_j_means[k]=mean_sim
+                # mean over all the k's sentences
+                layers_sim[i][j]=np.mean(i_j_means)
+                # mul by k to save the number of examples:
 
 
+    return layers_sim
 
 def find_batch_similarities(senteneces: List[List[str]],mask_loc: int, masked_word: str):
     # convert the token str to token_id:
@@ -162,7 +165,7 @@ def mask_word(sentence,location):
     sentence[location]=tokenizer.mask_token
     return sentence
 
-def create_table_from_results(words_dict):
+def create_table_from_results(words_dict,sentences_list):
     results = []
     for word in words_dict:
         for mask_loc in words_dict[word]:
@@ -180,17 +183,35 @@ def create_table_from_results(words_dict):
     df = pd.DataFrame(results)
     df.to_csv('/home/itay.nakash/projects/smooth_language/expirement_result.csv', index=False)    
 
-def compare_layers(words_dict):
-    results = []
+def compare_layers(words_dict,sentences_list,num_of_iter=-1):
+    num_of_states=13
+    layers_sim=np.zeros((num_of_states,num_of_states))
+    num_of_sentences=0
     for word in words_dict:
         for mask_loc in words_dict[word]:
             current_sentences=[mask_word(sentences_list[i],int(mask_loc)) for i in words_dict[word][mask_loc]]
-            compare_layers_similarities(current_sentences,mask_loc,word)
+            num_of_sentences+=len(current_sentences)
+            layers_sim+=len(current_sentences)*compare_layers_similarities(current_sentences,mask_loc,word)
+            num_of_iter-=1
+            if num_of_iter == 0:
+                layers_sim=layers_sim/num_of_sentences
+                return layers_sim
+    layers_sim=layers_sim/num_of_sentences
+    return layers_sim
+
+def write_dict_to_excel(layers_sims ,states_num:int):
+    results=[]
+    for i in range(states_num):
+        for j in range(states_num):
+            if j<i:
+                results.append({f'layers':f'({i},{j})',f'sim':layers_sims[i][j]})
+    df = pd.DataFrame(results)
+    df.to_csv('/home/itay.nakash/projects/smooth_language/layers_exp_result.csv', index=False)    
 
 if __name__ == "__main__":
-    most_freq_words=read_dict_from_json('n_most_frq')
-    word_to_sen=read_dict_from_json('word_to_sen_dict')
-    sentences_list=read_dict_from_json('sentences_list')
-
-    #create_table_from_results(word_to_sen)
-    compare_layers(word_to_sen)
+    word_to_sen=utils.read_dict_from_json('word_to_sen_dict')
+    sentences_list=utils.read_dict_from_json('sentences_list')
+    
+    #create_table_from_results(word_to_sen,sentences_list)
+    layers_sims=compare_layers(word_to_sen,sentences_list,5000)
+    write_dict_to_excel(layers_sims,STATES_NUM)
